0ece624f14cf5765c254a5198e0d701bf4715b20
kjun <keisoft.kjun@gmail.com>
2023-08-08 00:03:18 +0900

    GrpcChannelOptions 추가

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,10 +1,8 @@
-﻿using System.Threading.Tasks;
-
-using Grpc.Net.Client;
+﻿using Grpc.Net.Client;
 
 using GrpcClient;
 
-using var channel = GrpcChannel.ForAddress("http://localhost:5253");
+using var channel = GrpcChannel.ForAddress("http://localhost:5253", new GrpcChannelOptions { HttpHandler = new HttpClientHandler() });
 var client = new Greeter.GreeterClient(channel);
 var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
 Console.WriteLine("Greeting: " + reply.Message);
