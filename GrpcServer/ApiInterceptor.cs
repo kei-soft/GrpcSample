0d8d621e9796acd03414eec59b457d5b43f8e106
kjun <keisoft.kjun@gmail.com>
2024-04-25 10:46:25 +0900

    소스 정리

--- GrpcServer/ApiInterceptor.cs
+++ GrpcServer/ApiInterceptor.cs
@@ -7,8 +7,14 @@ namespace GrpcServer
     {
         public override Task ServerStreamingServerHandler<TRequest, TResponse>(TRequest request, IServerStreamWriter<TResponse> responseStream, ServerCallContext context, ServerStreamingServerMethod<TRequest, TResponse> continuation)
         {
+            // 옵션 설정 및 요청을 처리하기 전 로직
             context.WriteOptions = new WriteOptions(WriteFlags.BufferHint);
-            return base.ServerStreamingServerHandler(request, responseStream, context, continuation);
+
+            var response = base.ServerStreamingServerHandler(request, responseStream, context, continuation);
+
+            // 요청이 성공적으로 처리된 후 로직
+
+            return response;
         }
     }
 }
